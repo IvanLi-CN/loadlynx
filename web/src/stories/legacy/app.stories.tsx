5bd465e8ac5b5bf8e77ed6af33002e5be0a516c5
Ivan Li <ivanli2048@gmail.com>
2025-12-23 10:52:00 +0800

    fix(storybook): make legacy app story indexable
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- web/src/stories/legacy/app.stories.tsx
+++ web/src/stories/legacy/app.stories.tsx
@@ -1,12 +1,8 @@
-import type { Meta, StoryObj } from "@storybook/react";
 import { App } from "../../app.tsx";
 
-const meta = {
+export default {
   title: "Legacy/App (scaffold)",
   component: App,
-} satisfies Meta<typeof App>;
+};
 
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {};
+export const Default = {};
