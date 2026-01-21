1e648b9e5778d1ab86e20b9241b8d73939f76ded
Ivan Li <ivanli2048@gmail.com>
2026-01-21 20:32:39 +0800

    feat(web): add version link in console layout
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- web/src/env.d.ts
+++ web/src/env.d.ts
@@ -10,6 +10,10 @@ interface ImportMetaEnv {
   readonly DEV: boolean;
   readonly VITE_ENABLE_MOCK_BACKEND?: string;
   readonly VITE_USE_HTTP_BACKEND?: string;
+  readonly VITE_APP_VERSION?: string;
+  readonly VITE_APP_GIT_SHA?: string;
+  readonly VITE_APP_GIT_TAG?: string;
+  readonly VITE_GITHUB_REPO?: string;
 }
 
 // biome-ignore lint/correctness/noUnusedVariables: Used by Vite/TypeScript global typings.
