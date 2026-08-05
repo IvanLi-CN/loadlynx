1bae3ff9a490d651df127d0dd5e5296f96905395
Ivan Li <ivanli2048@gmail.com>
2026-08-05 10:18:23 +0800

    test(web): fix version evidence bounds
    Signed-off-by: Ivan Li <ivanli2048@gmail.com>

--- web/src/stories/components/app-version-link.stories.tsx
+++ web/src/stories/components/app-version-link.stories.tsx
@@ -28,7 +28,7 @@ export const Release: Story = {
 export const ReleaseVisual: Story = {
   render: (args) => (
     <div
-      className="inline-flex bg-base-100 px-2 pt-[5px] pb-[7px] text-base-content"
+      className="inline-flex w-[59.5px] bg-base-100 px-2 pt-[5px] pb-[7px] text-base-content"
       data-testid="app-version-link-evidence"
     >
       <AppVersionLink {...args} />
